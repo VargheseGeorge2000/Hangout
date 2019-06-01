fc0d2b207fd52676ac609481eed90315d4bce714
Varghese <varghese1george@gmail.com>
2019-06-01 00:03:48 -0400

    Fixing Settings/Modifying Models
    EST Time Zone / Changing models to User objects instead of text fields

--- hangout/social/models.py
+++ hangout/social/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 
+class Events(models.Model):
+    name = models.CharField(max_length=30)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_planned = models.DateField()
+    location = models.CharField(max_length=100)
+    cost_rating = models.CharField(max_length=100)
+
+
+class Memories(models.Model):
+    image = models.ImageField(blank=False)
+    caption = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
 class Groups(models.Model):
     name = models.CharField(max_length=30)
-    # Later on change to users maybe? But we'll stick with usernames as charfields
-    # members = models.ManyToManyField(models.User)
-    # manager = models.ForeignKey(models.User)
-    manager = models.CharField(max_length=30)
-    members = models.CharField(max_length=30)
+    members = models.ManyToManyField(models.User)
+    manager = models.ForeignKey(models.User)
+    events = models.ManyToManyField(Events)
+    memories = models.ManyToManyField(Memories)
